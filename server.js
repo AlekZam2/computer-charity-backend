b3d2f447498f81b4565780e58ad9ccbcb746572b
AlekZam2 <Lex.zam@hotmail.com>
2024-12-01 21:35:26 +0000

    mongoDB connected

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
 
 const app = express();
-
+dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Computer Charity API!");
